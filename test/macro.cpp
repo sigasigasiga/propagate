f8b5b3b8692de53a1f6c0c7ed80f4c4c8b86f8a2
Egor Bychin <e.siga@proton.me>
2024-10-14 15:10:49 +0400

    add `std::expected` test to `test/macro.cpp`

--- test/macro.cpp
+++ test/macro.cpp
@@ -1,23 +1,64 @@
+#include <string>
+
 #include <siga/propagate.hpp>
 
-std::optional<double> g_opt;
+struct my_error_type
+{};
 
-std::optional<double> try_plus_one()
+std::expected<int, my_error_type> g_ex;
+
+std::expected<std::string, my_error_type> try_ex_to_string()
 {
-    return SIGA_PROPAGATE_TRY(g_opt) + 1;
+    return std::to_string(SIGA_PROPAGATE_TRY(g_ex));
+}
+
+void test_ex()
+{
+    g_ex = std::unexpected{my_error_type{}};
+
+    auto v1 = try_ex_to_string();
+    if(v1.has_value()) {
+        abort();
+    }
+
+    g_ex = 777;
+    auto v2 = try_ex_to_string();
+    if(v2 != "777") {
+        abort();
+    }
+}
+
+// -----------------------------------------------------------------------------
+
+std::optional<int> &get_opt()
+{
+    static std::optional<int> ret;
+    return ret;
+}
+
+std::optional<std::string> try_opt_to_string()
+{
+    return std::to_string(SIGA_PROPAGATE_TRY(get_opt()));
+}
+
+void test_opt()
+{
+    get_opt() = std::nullopt;
+
+    auto v1 = try_opt_to_string();
+    if(v1.has_value()) {
+        abort();
+    }
+
+    get_opt() = 777;
+    auto v2 = try_opt_to_string();
+    if(v2 != "777") {
+        abort();
+    }
 }
 
 int main()
 {
-    auto v1 = try_plus_one();
-    if(v1) {
-        abort();
-    }
-
-    g_opt = 1;
-
-    auto v2 = try_plus_one();
-    if(v2 != 2.0) {
-        abort();
-    }
+    test_ex();
+    test_opt();
 }
